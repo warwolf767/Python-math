570888bcb5f6d63ff222559ef82814e18cc5c122
warwolf767 <warwolf767@users.noreply.github.com>
2016-07-27 14:07:18 +0800

    Create mod1.py
    num * num_r = 1 mod mod_num

---
+++ mod1.py
@@ -0,0 +1,17 @@
+
+#-*-coding:utf8-*-
+
+# num * num_r = 1 mod mod_num
+def mod1(num, mod_num):
+	"""
+	:type num: int
+	:type mod_num: int
+	:rtype: int
+	"""		
+	i = 1
+	while True:
+		if((mod_num * i + 1) % num == 0):
+			num_r = (mod_num * i + 1) / num
+			break
+		i = i + 1
+	return num_r
