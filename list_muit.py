8a0b4851c6938b56781d47d5763f7948b0b18071
warwolf767 <warwolf767@users.noreply.github.com>
2016-07-27 14:06:23 +0800

    Create list_muit.py
    The product of all list elements

---
+++ list_muit.py
@@ -0,0 +1,13 @@
+
+#-*-coding:utf8-*-
+
+# The product of all list elements
+def list_muit(list_o):
+	"""
+	:type list_o: list[int]
+	:rtype: int
+	"""		
+	muit_sum = 1
+	for x in list_o:
+		muit_sum *= x
+	return muit_sum	
