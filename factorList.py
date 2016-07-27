cb3eb2e68131ed3eacf94e411e5cc4880a3e3d28
warwolf767 <warwolf767@users.noreply.github.com>
2016-07-27 14:01:47 +0800

    Create factorList.py
    the factor list of num

---
+++ factorList.py
@@ -0,0 +1,21 @@
+
+#-*-coding:utf8
+
+import math
+
+# the factor list of num
+def factorList(num):
+	"""
+	:type num: int
+	:rtype: list[int]
+	"""		
+	factor_list = []
+	for n in xrange(2, int(math.sqrt(num)) + 1):
+		while num % n == 0:
+			factor_list.append(n)
+			num = num / n
+	if factor_list == []:
+		factor_list.append(1)
+	if not num == 1: 
+		factor_list.append(num)	
+	return factor_list
